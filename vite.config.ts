063d76ce4dbe56b4f51e0961996588b315f5f2f3
Nadifdzaikra <nadifdzaikra@gmail.com>
2024-10-25 23:25:01 +0700

    fix: delpoy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://nadifdzaikra.github.io/pokemon/",
+  base: "/pokemon/",
   build: {
     outDir: "build",
   },
