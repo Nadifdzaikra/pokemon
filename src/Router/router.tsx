ebdd928d512613712953c71eb9694929fdbde59d
Nadifdzaikra <nadifdzaikra@gmail.com>
2024-10-25 23:02:57 +0700

    fix: blank white

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -9,37 +9,40 @@ import ContextPage from "../Pages/Context";
 // Status autentikasi dari localStorage
 // const isAuthenticated = localStorage.getItem("authToken") !== null;
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      // <ProtectedRoute isAuthenticated={isAuthenticated}>
-      <LayoutMain />
-      // </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/dashboard",
-        element: <HomePage />,
-      },
-      {
-        path: "/context",
-        element: <ContextPage />,
-      },
-    ],
-  },
-  //   {
-  //     path: "/login",
-  //     element: isAuthenticated ? <Navigate to="/" /> : <LoginPage />,
-  //   },
-  {
-    path: "*",
-    element: <NotFoundPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        // <ProtectedRoute isAuthenticated={isAuthenticated}>
+        <LayoutMain />
+        // </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/dashboard",
+          element: <HomePage />,
+        },
+        {
+          path: "/context",
+          element: <ContextPage />,
+        },
+      ],
+    },
+    //   {
+    //     path: "/login",
+    //     element: isAuthenticated ? <Navigate to="/" /> : <LoginPage />,
+    //   },
+    {
+      path: "*",
+      element: <NotFoundPage />,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 export default router;
